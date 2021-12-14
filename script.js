4006caf07e2ba5c673b74f2061933139e3146ba5
i-doshechnikow <iliya.yurievich@yandex.ru>
2021-12-14 22:21:24 +0300

    add html and init js script

---
+++ script.js
@@ -0,0 +1,40 @@
+const draggable_list = document.getElementById("draggable-list");
+const check = document.getElementById("check");
+
+const richestPeople = [
+  "person 1",
+  "person 2",
+  "person 3",
+  "person 4",
+  "person 5",
+  "person 6",
+  "person 7",
+  "person 8",
+  "person 9",
+  "person 10",
+];
+
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+function createList() {
+  [...richestPeople].forEach((person, index) => {
+    const listItem = document.createElement("li");
+    listItem.setAttribute("data-index", index);
+    listItem.innerHTML = `<span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-lines"></i>
+    </div>`;
+
+    listItems.push(listItem);
+
+    draggable_list.appendChild(listItem);
+  });
+}
+
+console.log("draggable_list :>> ", draggable_list);
+console.log("check :>> ", check);
