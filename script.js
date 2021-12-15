15601eb7624f989b9cd8cb4bdbb82f8fa5068ae4
i-doshechnikow <iliya.yurievich@yandex.ru>
2021-12-15 23:13:57 +0300

    stylised component

--- script.js
+++ script.js
@@ -21,20 +21,24 @@ let dragStartIndex;
 createList();
 
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
-    listItem.setAttribute("data-index", index);
-    listItem.innerHTML = `<span class="number">${index + 1}</span>
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
+      listItem.setAttribute("data-index", index);
+      listItem.innerHTML = `<span class="number">${index + 1}</span>
     <div class="draggable" draggable="true">
         <p class="person-name">${person}</p>
         <i class="fas fa-grip-lines"></i>
     </div>`;
 
-    listItems.push(listItem);
+      listItems.push(listItem);
 
-    draggable_list.appendChild(listItem);
-  });
+      draggable_list.appendChild(listItem);
+    });
 }
 
-console.log("draggable_list :>> ", draggable_list);
-console.log("check :>> ", check);
+// console.log("draggable_list :>> ", draggable_list);
+// console.log("check :>> ", check);
